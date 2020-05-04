fc1689b5275e757c5d21ad8df83b09de4f415a7a
m47h4r <m47h4r@gmail.com>
2020-05-04 15:02:49 +0430

    add incremental quote setting

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import quotes from "../../data/quote";
 
 import "./index.css";
@@ -14,23 +14,41 @@ function Main() {
   const [author, setAuthor] = useState("");
   const [belongsTo, setBelongsTo] = useState("");
 
+  useEffect(() => {});
+
+  function setQuoteIncrementally(quoteArray) {
+    if (quoteArray.length <= 0) {
+      return;
+    }
+    const [currentWord, ...rest] = quoteArray;
+    setText((old) => [...old, " " + currentWord]);
+			const timer = setTimeout(() => {
+				setQuoteIncrementally(rest);
+			}, 50);
+			return () => clearTimeout(timer);
+  }
+
   function setQuote() {
     const quote = quotes[generateRandomNumber()];
-    setText(quote.text);
+    const quoteTextArray = quote.text.split(" ");
+    console.log(quoteTextArray);
+		setText('');
+    setQuoteIncrementally(quoteTextArray);
+    //setText(quote.text);
     setAuthor(quote.author);
     setBelongsTo(quote.belongsTo);
   }
 
   return (
     <div className="app">
-        <div className="quote">
-          <div className="quote__text">{text}</div>
-          <div className="quote__author">{author}</div>
-          <div className="quote__belongs-to">{belongsTo}</div>
-        </div>
-        <button className="quote-generator" onClick={setQuote}>
-          Generate a quote!
-        </button>
+      <div className="quote">
+        <div className="quote__text">{text}</div>
+        <div className="quote__author">{author}</div>
+        <div className="quote__belongs-to">{belongsTo}</div>
+      </div>
+      <button className="quote-generator" onClick={setQuote}>
+        Generate a quote!
+      </button>
     </div>
   );
 }
